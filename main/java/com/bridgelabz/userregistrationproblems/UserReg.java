f512c1dd52352752b80cd56f77c08d6602ca64a3
shindeanant <shindeanant1997@gmail.com>
2021-10-26 01:03:41 +0530

    Added: Validate user .

--- main/java/com/bridgelabz/userregistrationproblems/UserReg.java
+++ main/java/com/bridgelabz/userregistrationproblems/UserReg.java
@@ -7,53 +7,51 @@ public class UserReg {
 	public static final Scanner sc = new Scanner(System.in);
 	public final String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
 	public final String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
-	public final String emailPattern = "[a-z0-9_]*[.a-z0-9_]*?@[a-z]*.[a-z]*[.a-z]?$";
+	public final String emailPattern = "[a-z0-9_+-]*(.[a-z0-9_+-]*?)@[a-z0-9]*.[a-z].*?[a-z]*?$";
 	public final String phonePattern = "^[0-9]{2}[0-9]{10}$";
-	public final String passwordPattern = "^.{8,}";
+	public final String passordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8,}$";
 
-	/*
-	 * / This method checks if the entered first name is valid
-	 */
+	// This method checks if the entered first name is valid
 	public boolean validateFirstName(String fname) {
 		Pattern pattern = Pattern.compile(firstNamePattern);
 		return pattern.matcher(fname).matches();
 	}
 
-	/*
-	 * / This method checks if the entered lawst name is valid
-	 */
+	// This method checks if the entered lawst name is valid
 	public boolean validateLastName(String lname) {
 		Pattern pattern = Pattern.compile(firstNamePattern);
 		return pattern.matcher(lname).matches();
 	}
 
-	/*
-	 * / This method checks if the entered email is valid
-	 */
+	// This method checks if the entered email is valid
 	public boolean validateEmail(String email) {
 		Pattern pattern = Pattern.compile(emailPattern);
 		return pattern.matcher(email).matches();
 	}
 
-	/*
-	 * / This method checks if the entered phone number is valid
-	 */
+	// This method checks if the entered phone number is valid
 	public boolean validatePhone(String phone) {
 		Pattern pattern = Pattern.compile(phonePattern);
 		return pattern.matcher(phone).matches();
 	}
 
-	/*
-	 * / This method checks if the entered password is valid
-	 */
+	// This method checks if the entered password is valid
 	public boolean validatePassword(String password) {
-		Pattern pattern = Pattern.compile(passwordPattern);
+		Pattern pattern = Pattern.compile(passordPattern);
 		return pattern.matcher(password).matches();
 	}
 
-	/*
-	 * / This is the main function
-	 */
+	// Method for printing validate email output;
+	public void emailTest(String email) {
+		System.out.print(email + "->");
+		if (validateEmail(email)) {
+			System.out.printf("Email Valid \n");
+		} else {
+			System.out.printf("Email Invalid \n");
+		}
+	}
+
+	// This is the main function
 	public static void main(String[] args) {
 
 		UserReg obj = new UserReg();
@@ -90,13 +88,25 @@ public class UserReg {
 			System.out.println("Phone No. Invalid");
 		}
 
-		System.out.println("\nEnter Password (Min 8 characters): ");
+		System.out.println(
+				"\nEnter Password (Min 8 characters with Atleast 1 Uppercase, 1 Numeric Digit, 1 Special Character): ");
 		String password = sc.next();
 		if (obj.validatePassword(password)) {
 			System.out.println("Password Valid");
 		} else {
 			System.out.println("Password Invalid");
 		}
+
+		System.out.println("\nSamples Emails to Test");
+		obj.emailTest("abc@yahoo.com");
+		obj.emailTest("abc-100@yahoo.com");
+		obj.emailTest("abc.100@yahoo.com");
+		obj.emailTest("abc111@abc.com");
+		obj.emailTest("abc-100@abc.net");
+		obj.emailTest("abc.100@abc.com.au");
+		obj.emailTest("abc@1.com");
+		obj.emailTest("abc@gmail.com.com");
+		obj.emailTest("abc+100@gmail.com");
 	}
 
 }
